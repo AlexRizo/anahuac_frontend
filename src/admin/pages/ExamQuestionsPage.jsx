1c96577bc66d834b857ea43ab8a7b1a6a6165faf
Alex <david_alejandrorizo@outlook.com>
2025-02-11 13:32:38 -0600

    score -> exam

--- src/admin/pages/ExamQuestionsPage.jsx
+++ src/admin/pages/ExamQuestionsPage.jsx
@@ -3,16 +3,16 @@ import { QuestionsTable } from "../components"
 export const ExamQuestionsPage = () => {
     return (
         <main className="w-full">
-        <div>
-            <nav className="flex items-center gap-5 px-14 py-5 2xl:py-8">
-                <h1 className="text-2xl 2xl:text-3xl font-semibold">Exámenes</h1>
-                <p className="text-gray-400 pt-1">/ Contenido examen <strong>PREPARATORIA</strong></p>
-            </nav>
-            <hr />
-        </div>
-        <div className="px-14 py-6">
-            <QuestionsTable />
-        </div>
-    </main>
+            <div>
+                <nav className="flex items-center gap-5 px-14 py-5 2xl:py-8">
+                    <h1 className="text-2xl 2xl:text-3xl font-semibold">Exámenes</h1>
+                    <p className="text-gray-400 pt-1">/ Contenido examen <strong></strong></p>
+                </nav>
+                <hr />
+            </div>
+            <div className="px-14 py-6">
+                <QuestionsTable />
+            </div>
+        </main>
     )
 }
--- src/store/exam/examSlice.js
+++ src/store/exam/examSlice.js
@@ -10,7 +10,7 @@ const initialState = {
     isLoading: 'loading',
     exam_level: 0,
     exams: [],
-    scorePerExam: {},
+    exam: null,
 };
 
 export const examSlice = createSlice({
@@ -22,7 +22,7 @@ export const examSlice = createSlice({
             state.total = payload.length;
         },
         setScorePerExam: (state, { payload }) => {
-            state.scorePerExam = payload;
+            state.exam = payload;
         },
         onSetActiveQuestion: (state, { payload }) => {
             state.activeQuestion = payload;
@@ -41,7 +41,7 @@ export const examSlice = createSlice({
             state.isLoading = payload;
             if (payload === 'loading') {
                 state.exams = [];
-                state.scorePerExam = {};
+                state.exam = {};
             }
         },
         onRestartExam: (state) => {
@@ -49,7 +49,7 @@ export const examSlice = createSlice({
             state.specials = [];
             state.total = 0;
             state.totalResponded = 0;
-            state.scorePerExam = {};
+            state.exam = null;
         },
         onLoadExams: (state, { payload }) => {
             state.exams = payload;
